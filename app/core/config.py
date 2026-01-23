5717f9656b25d84bad1de1a16ceb49228c1b0950
ravee360 <ravikantprajapati232326@gmail.com>
2026-01-23 18:08:31 +0530

    added app

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+''' Loads environment variable and app-wide settings'''
+import os
+from dotenv load dotenv
+
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME = 'Car Price API'
+    API_KEY = os.getenv('API_KEY','demo-key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY','secret')
+    JWT_ALGORITHM = 'HS256'
+    REDIS_URL = os.getenv('REDIS_URL','redis://localhost:6379')
+    MODEL_PATH = 'app/models/model.pkl'
+    
